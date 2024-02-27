1e33534eded90314861d80985786560f329338a3
LEE <dlgmldnd017@naver.com>
2024-02-27 19:03:31 +0900

    Fix : 테스트 어노테이션 삭제

--- server/src/test/java/com/ssafy/server/ServerApplicationTests.java
+++ server/src/test/java/com/ssafy/server/ServerApplicationTests.java
@@ -1,12 +1,6 @@
 package com.ssafy.server;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ServerApplicationTests {
-
-    @Test
     void contextLoads() {
     }
 }
